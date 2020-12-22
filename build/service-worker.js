8fe84caa365f2eb8ac225075a1e3f1239d8b712d
user <user@users-MacBook-Pro.local>
2020-12-22 11:06:11 +0100

    Changes to about us

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c4fa53783fa590eed1ca24ee0df7489.js"
+  "/precache-manifest.9ff1b4c048535d27bd63ddffc28d187e.js"
 );
 
 self.addEventListener('message', (event) => {
